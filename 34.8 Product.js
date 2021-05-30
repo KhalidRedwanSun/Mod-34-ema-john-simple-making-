5c1769d73a09914828dd1b03bb6b4920ce2da73c
KhalidRedwanSun <77046957+KhalidRedwanSun@users.noreply.github.com>
2021-05-31 00:50:03 +0600

    Create 34.8 Product.js

---
+++ 34.8 Product.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Product = (props) => {
+    
+    return (
+        <div>
+            <h1>{props.product.name}</h1>
+            
+        </div>
+    );
+};
+
+export default Product;
